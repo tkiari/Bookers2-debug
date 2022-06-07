6a39f7239b815795a6b55e8da1f1c5d6793ba27b
EC2 Default User <ec2-user@ip-172-31-26-83.ec2.internal>
2022-06-07 08:57:07 +0000

    [Finish]ouyou5

--- config/routes.rb
+++ config/routes.rb
@@ -15,6 +15,7 @@ Rails.application.routes.draw do
   end
 
   get "home/about"=>"homes#about"
+  get "search" => "searches#search", as: "search"
 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
