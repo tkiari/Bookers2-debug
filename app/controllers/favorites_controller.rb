893a48c137dc91453958d06461c5d678a7ce3dbf
EC2 Default User <ec2-user@ip-172-31-26-83.ec2.internal>
2022-06-11 09:12:39 +0000

    [Finish] ouyou 6

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -1,16 +1,16 @@
 class FavoritesController < ApplicationController
  def create
    book = Book.find(params[:book_id])
-   favorite = current_user.favorites.new(book_id: params[:book_id])
-   favorite.save
-   redirect_to books_path
+   @favorite = current_user.favorites.new(book_id: params[:book_id])
+   @favorite.save
+
  end
 
  def destroy
    book = Book.find(params[:book_id])
-   favorite = current_user.favorites.find_by(book_id: params[:book_id])
-   favorite.destroy
-   redirect_to books_path
+   @favorite = current_user.favorites.find_by(book_id: params[:book_id])
+   @favorite.destroy
+
  end
- 
+
 end
